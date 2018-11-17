1dae61419a66775816dc5a814de5ce94d4c29f61
Mike Neilens <mike.neilens@gmail.com>
2018-11-17 21:47:24 +0000

    added mysql connection to test

--- GitLabUsers/test.php
+++ GitLabUsers/test.php
@@ -1,3 +1,21 @@
 <?php
 echo "Hello world!";
+
+
+$servername = MYSQL_DSN;
+$username = MYSQL_USER;
+$password = MYSQL_PASSWORD;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+$conn->close();
+
+//$sql = "CREATE TABLE SearchCache (search char(50) NOT NULL, fullSearch char(200) NOT NULL, json varchar(4000) NOT NULL, create_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, uId char(16) NOT NULL )";
+
 ?>
