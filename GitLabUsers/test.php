7d6f16cdb8a23fbc35825c441a5befcb3fe1db9b
Mike Neilens <mike.neilens@gmail.com>
2018-11-17 22:00:16 +0000

    trying to get a mysql conn

--- GitLabUsers/test.php
+++ GitLabUsers/test.php
@@ -2,19 +2,22 @@
 echo "Hello world!";
 
 
-$servername = MYSQL_DSN;
+$dsn = MYSQL_DSN;
 $username = MYSQL_USER;
 $password = MYSQL_PASSWORD;
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+try {
+    $conn = new PDO($dsn, $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    $conn->close();
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-$conn->close();
 
 //$sql = "CREATE TABLE SearchCache (search char(50) NOT NULL, fullSearch char(200) NOT NULL, json varchar(4000) NOT NULL, create_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, uId char(16) NOT NULL )";
 
