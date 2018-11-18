d7af92c71ab7ecfc8695bca6b81e37db5e4c0be7
Mike Neilens <mike.neilens@gmail.com>
2018-11-18 14:09:46 +0000

    fix

--- GitLabUsers/connection.php
+++ GitLabUsers/connection.php
@@ -1,7 +1,21 @@
 <?php
 
 function getConnection() {
-	
+	$dsn = "mysql:unix_socket=/cloudsql/gitlabusers-mn2017:europe-west1:test1;dbname=dbtest";
+	$username = "mike1";
+	$password = "mike1";
+
+	try {
+    	$conn = new PDO($dsn, $username, $password);
+    	// set the PDO error mode to exception
+    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	return $conn;
+    }
+	catch(PDOException $e)
+    {
+    	echo "Connection failed: " . $e->getMessage();
+    	return false;
+    }
 }
 
 ?>
