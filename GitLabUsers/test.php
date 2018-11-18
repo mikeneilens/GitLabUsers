c3259fd6013ae4cab9c55f349e5872b0379eb954
Mike Neilens <mike.neilens@gmail.com>
2018-11-18 14:07:26 +0000

    added connection module

--- GitLabUsers/test.php
+++ GitLabUsers/test.php
@@ -1,28 +1,13 @@
 <?php
-echo "Hello world!\n";
+echo "Hello world! \n";
 
+include_once "connection.php";
 
-$dsn = "mysql:unix_socket=/cloudsql/gitlabusers-mn2017:europe-west1:test1;dbname=dbtest";
-$username = "mike1";
-$password = "mike1";
+$conn = getConnection();
 
-echo "MYSQL_DSN $dsn \n";
-echo "MYSQL_USER $username \n";
-echo "MYSQL_PASSWORD $password \n";
-
-try {
-    $conn = new PDO($dsn, $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    $conn->close();
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-
-
+if ($conn == false) {
+    echo "connection failed";
+}
 //$sql = "CREATE TABLE SearchCache (search char(50) NOT NULL, fullSearch char(200) NOT NULL, json varchar(4000) NOT NULL, create_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, uId char(16) NOT NULL )";
 
 ?>
